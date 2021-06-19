6e1d2c932a01360da7f8df451ab319370638eb7b
Rishitha Pericherla <pericherla.rishitha@gmail.com>
2021-06-18 23:27:16 -0700

    modified css file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Home from './Home';
-
+var time = new Date();
+const hours = time.toLocaleString('en-US', { hour: 'numeric', hour12: true })
+ 
 function App() {
   return (
     <div className="App">
       <h1>ColourLovers. <strong>Live.</strong></h1>
-
+       <strong><p>Last Updated at {hours}</p></strong>
       <Home/>
     </div>
   );
